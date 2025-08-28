12d8867e9097d8ac1d6e64ec83e3dd18643766a1
sohith <102178665+Sohith2007@users.noreply.github.com>
2025-08-28 22:39:39 +0530

    Add files via upload

---
+++ 800 Rated CP-31 Sheet/1866A Ambitious Kid.cpp
@@ -0,0 +1,87 @@
+/*
+Chaneka, Pak Chanek's child, is an ambitious kid, so Pak Chanek gives her the following problem to test her ambition.
+
+Given an array of integers [A1,A2,A3,…,AN]
+. In one operation, Chaneka can choose one element, then increase or decrease the element's value by 1
+. Chaneka can do that operation multiple times, even for different elements.
+
+What is the minimum number of operations that must be done to make it such that A1×A2×A3×…×AN=0
+?
+
+Input
+The first line contains a single integer N
+ (1≤N≤105
+).
+
+The second line contains N
+ integers A1,A2,A3,…,AN
+ (−105≤Ai≤105
+).
+
+Output
+An integer representing the minimum number of operations that must be done to make it such that A1×A2×A3×…×AN=0
+.
+
+Examples
+InputCopy
+3
+2 -6 5
+OutputCopy
+2
+InputCopy
+1
+-3
+OutputCopy
+3
+InputCopy
+5
+0 -1 0 1 0
+OutputCopy
+0
+Note
+In the first example, initially, A1×A2×A3=2×(−6)×5=−60
+. Chaneka can do the following sequence of operations:
+
+Decrease the value of A1
+ by 1
+. Then, A1×A2×A3=1×(−6)×5=−30
+Decrease the value of A1
+ by 1
+. Then, A1×A2×A3=0×(−6)×5=0
+In the third example, Chaneka does not have to do any operations, because from the start, it already holds that A1×A2×A3×A4×A5=0×(−1)×0×1×0=0
+*/
+
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int mini = INT_MAX;
+    bool haszero = false;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        if (x == 0)
+        {
+            haszero = true;
+        }
+        else
+        {
+            mini = min(mini, abs(x));
+        }
+    }
+    if (haszero)
+    {
+        cout << 0;
+    }
+    else
+    {
+        cout << mini;
+    }
+    return 0;
+}
